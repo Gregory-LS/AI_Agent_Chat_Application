7eba183526c1a5a4f51e0afc41da057085972fe0
Gregory-LS <118018265+Gregory-LS@users.noreply.github.com>
2026-09-02 08:35:11 +0100

    Reinit: remove src/karbonite/__init__.py

--- src/karbonite/__init__.py
+++
@@ -1,8 +0,0 @@
-from .extras import RichProgress, run_parallel
-from .core import process_data  # assumed existing core module
-
-__all__ = [
-    "RichProgress",
-    "run_parallel",
-    "process_data",
-]
