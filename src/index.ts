e8938c6e4a61aa84d8ccd2592eb10d67cab52c58
Gregory-LS <118018265+Gregory-LS@users.noreply.github.com>
2026-09-02 08:35:10 +0100

    Reinit: remove src/index.ts

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { ModelPicker } from './components/ModelPicker';
-export type { ModelPickerProps } from './components/ModelPicker';
-export { useModels } from './hooks/useModels';
-export type { UseModelsOptions } from './hooks/useModels';
-export { fetchModels } from './api/modelsApi';
-export type { Model } from './types';
